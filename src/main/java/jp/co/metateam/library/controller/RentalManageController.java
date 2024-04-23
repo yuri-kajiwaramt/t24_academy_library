233d853b8e988b1756fa798683f5dfc93553c2ad
yuri-kajiwaramt <yuri.kajiwara@metateam.co.jp>
2024-04-23 21:01:02 +0900

    貸出一覧、貸出登録機能追加

--- src/main/java/jp/co/metateam/library/controller/RentalManageController.java
+++ src/main/java/jp/co/metateam/library/controller/RentalManageController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import jp.co.metateam.library.service.AccountService;
 import jp.co.metateam.library.service.RentalManageService;
 import jp.co.metateam.library.service.StockService;
+import jp.co.metateam.library.model.RentalManage;
+import jp.co.metateam.library.model.Account;
+import jp.co.metateam.library.model.Stock;
+import jp.co.metateam.library.values.RentalStatus;
+import jp.co.metateam.library.model.RentalManageDto;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -40,11 +45,49 @@ public class RentalManageController {
     @GetMapping("/rental/index")
     public String index(Model model) {
         // 貸出管理テーブルから全件取得
-
+        List <RentalManage> rentalManageList = this.rentalManageService.findAll();
         // 貸出一覧画面に渡すデータをmodelに追加
-
+        model.addAttribute("rentalManageList", rentalManageList);
         // 貸出一覧画面に遷移
         return "";
+        return "rental/index";
+    }
+    @GetMapping("/rental/add")
+    public String add(Model model) {
+        List<RentalManage> rentalManageList = this.rentalManageService.findAll();
+        List<Account> accountList = this.accountService.findAll();
+        List<Stock> stockList = this.stockService.findAll();
+
+        model.addAttribute("accounts",accountList);
+        model.addAttribute("stockList", stockList);
+        model.addAttribute("rentalStatus", RentalStatus.values());
+
+        if (!model.containsAttribute("rentalManageDto")) {
+            model.addAttribute("rentalManageDto", new RentalManageDto());
+        }
+
+        return "rental/add";
+    }
+
+    @PostMapping("/rental/add")
+    public String save(@Valid @ModelAttribute RentalManageDto rentalManageDto, BindingResult result, RedirectAttributes ra) {
+        try {
+            if (result.hasErrors()) {
+                throw new Exception("Validation error.");
+            }
+            // 登録処理
+            this.rentalManageService.save(rentalManageDto);
+
+            return "redirect:/rental/index";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            ra.addFlashAttribute("rentalManageDto", rentalManageDto);
+            ra.addFlashAttribute("org.springframework.validation.BindingResult.rentalManageDto", result);
+
+            return "redirect:/rental/add";
+        }
+    
     }
 
 }
